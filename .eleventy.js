60b73883af0955333380128c64e0d978f95d46eb
itsmeat <itsmeat@duck.com>
2024-03-21 16:45:53 -0400

    Changed to webc components

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,25 @@
 module.exports = (eleventyConfig)=>{
+    eleventyConfig.addPlugin(require("@11ty/eleventy-plugin-webc"),{
+        components:"src/_includes/*.webc"
+    });
+
     eleventyConfig.addWatchTarget('./styles/tailwindconfig.js');
     eleventyConfig.addWatchTarget('./styles.tailwinds.css');
+    eleventyConfig.setBrowserSyncConfig({
+        callbacks:{
+            ready:(err, bs)=>{
+                bs.addMiddleware("*", (req, res)=>{
+                    const content_404 = readFileSync('_site/404.html');
+                    res.write(content_404);
+                    res.writeHead(404);
+                    res.end()
+                })
+            }
+        }
+    });
+    return{
+        dir:{
+            input: "src"
+        }
+    }
 }
