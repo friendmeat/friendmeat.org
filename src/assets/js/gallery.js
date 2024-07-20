84ccdc0a9b86648ffc722a12edee275d8a44b1ba
friendmeat <itsmeat@duck.com>
2024-07-20 12:39:23 -0400

    Added gallery features

--- src/assets/js/gallery.js
+++ src/assets/js/gallery.js
@@ -1,12 +1,42 @@
+function disableScroll() {
+    // Get the current page scroll position
+    scrollTop = document.documentElement.scrollTop;
+    scrollLeft = document.documentElement.scrollLeft,
+
+    // if any scroll is attempted,
+    // set this to the previous value
+    window.onscroll = function () {
+        window.scrollTo(scrollLeft, scrollTop);
+    };
+}
+
+function enableScroll() {
+    window.onscroll = function () { };
+}
+
 window.onload = function(){
     
     let shadowbox = document.querySelector("div#shadowbox");
-    console.log(shadowbox);
+    let shadowboxImage = shadowbox.querySelector("img");
+    console.log(shadowboxImage);
+    shadowbox.addEventListener("click", ()=>{
+        shadowbox.classList.replace("opacity-100", "opacity-0")
+        shadowbox.classList.toggle("pointer-events-none");
+
+        enableScroll()
+    })
 
     let galleryItems = document.querySelectorAll("div.gallery-item")
     galleryItems.forEach(item=>{
-        item.addEventListener("click", (event)=>{
-            console.log(event)
+        item.addEventListener("click", ()=>{
+            img = item.querySelector("img").attributes.src.value
+            shadowboxImage.setAttribute("src", img)
+            
+            shadowbox.classList.toggle("pointer-events-none");
+            shadowbox.classList.replace("opacity-0", "opacity-100")
+
+            
+            disableScroll()
         })
     })
 }
