9bbc32016a15d4d238d2b9498e5ddb3065f59766
itsmeat <itsmeat@duck.com>
2024-09-19 11:34:50 -0400

    Fixed blog links

--- src/blog/posts/posts.11tydata.js
+++ src/blog/posts/posts.11tydata.js
@@ -1,5 +1,5 @@
 module.exports={
     tags:["posts"],
     layout:"blog/post",
-    permalink:"{{ date | slugifyDate }}/{{ title | slugify }}"
+    permalink:"{{ date | slugifyDate }}/{{ title | slugify }}/"
 }
