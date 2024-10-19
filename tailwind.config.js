c2c004453c187ab60106c8dbc8127ecea926c264
friendmeat <itsmeat@duck.com>
2024-10-19 19:59:41 -0400

    Changed color scheme and tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ["src/**/*.{html,js,liquid,md,njk}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'background':'#0a0a0a',
+        'text':'#e5e7eb',
+        'accent':'#d9448a'
+      },
+    },
   },
   plugins: [],
 }
